056be71a50c6a6e9eaf109aa8f0a5739782f1173
Rafee Daanish <rafee.daanish@gmail.com>
2017-02-06 15:07:04 -0500

    added controllers

---
+++ Bookshelf1/Models/Book.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Bookshelf1.Models
+{
+    public class Book
+    {
+        [Key]
+        public int ID { get; set; }
+        public string Title { get; set; }
+        public DateTime PublishedDate { get; set; }
+        public string Description { get; set; }
+        public string ISBN { get; set; }
+
+        [ForeignKey("Category")]
+        
+        public int CategoryID { get; set; }
+        public virtual Category Category { get; set; }
+
+    }
+}
---
+++ Bookshelf1/Models/Bookshelf1Context.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace Bookshelf1.Models
+{
+    public class Bookshelf1Context : DbContext
+    {
+        // You can add custom code to this file. Changes will not be overwritten.
+        // 
+        // If you want Entity Framework to drop and regenerate your database
+        // automatically whenever you change your model schema, please use data migrations.
+        // For more information refer to the documentation:
+        // http://msdn.microsoft.com/en-us/data/jj591621.aspx
+    
+        public Bookshelf1Context() : base("name=Bookshelf1Context")
+        {
+        }
+
+        public System.Data.Entity.DbSet<Bookshelf1.Models.Book> Books { get; set; }
+
+        public System.Data.Entity.DbSet<Bookshelf1.Models.Category> Categories { get; set; }
+    }
+}
